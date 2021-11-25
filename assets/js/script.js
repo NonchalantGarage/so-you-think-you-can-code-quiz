36814c6d0ee45b10320837a7aa2441606abd8f0c
curtis <hong.curtis@gmail.com>
2021-11-25 10:54:57 -0800

    add timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,35 @@
 // Quiz Functionality
+
 var startButtonEl = document.getElementById('start-btn');
 
 startButtonEl.addEventListener('click',startGame);
 
 // 
-var startGame = function(){
-    console.log("hi")
+function startGame (){
+    var oneMinute = 60 *1,
+    display =document.getElementById("timerCountdown")
+    startTimer(oneMinute,display);
+
 }
+// Timer Logic 
+var startTimer = function(duration,display){
+    var timer= duration, minutes, seconds;
+    setInterval(function(){
+        minutes = parseInt(timer / 60,10);
+        seconds = parseInt(timer % 60,10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if(--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}        
+
+
 
 
 // Add a Timer that starts on click start quiz btn
