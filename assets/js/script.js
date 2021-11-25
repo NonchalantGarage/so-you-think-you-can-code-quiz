a5b80f6bb43cf2f8471646a40a9ff94cf1f26d6c
curtis <hong.curtis@gmail.com>
2021-11-25 10:06:23 -0800

    add Css

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Quiz Functionality
+var startButtonEl = document.getElementById('start-btn');
+
+startButtonEl.addEventListener('click',startGame);
+
+// 
+var startGame = function(){
+    console.log("hi")
+}
+
+
+// Add a Timer that starts on click start quiz btn
+
+
+// quiz ends when timer reaches 0 or finished answering all 5 questions  
+
+// Track score using local storage 
+
